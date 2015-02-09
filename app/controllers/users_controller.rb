fd503ba4b4f1bfe4e1f2608e665eb64b46db99ec
Maddy Lau <maddylau@gmail.com>
2015-02-08 23:51:09 -0500

    updated users controller with articles controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find_by_id(params[:id])
+    @article = Article.find_by_id(params[:id])
+    # @article = Article.where(user_id: 1)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207231333) do
+ActiveRecord::Schema.define(version: 20150209012349) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "article"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
